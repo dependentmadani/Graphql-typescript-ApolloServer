8a29ab81a55ce293533b64dca16670f0277122f9
dependentmadani <madani.badaoui12@gmail.com>
2024-06-29 01:02:03 +0100

    initialize the project

---
+++ src/datasource-rest/listing-api.ts
@@ -0,0 +1,11 @@
+require('dotenv').config();
+import { RESTDataSource } from "@apollo/datasource-rest";
+import { Listing } from "../types";
+
+export class ListingAPI extends RESTDataSource {
+    baseURL = process.env.BASEURL;
+
+    getFeaturedListings() {
+        return this.get<Listing[]>("feature-listings")
+    }
+}
